fb9ae4388c5fd56cb5d7e2253eac2f00cd98ee1a
David Alenoghena <davidalenoghena@gmail.com>
2024-09-03 15:58:41 +0100

    added link to check contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,7 +10,7 @@ async function main() {
     );
 
     const merkleRootFac = await ethers.getContractFactory("merkleroot");
-    await merkleRootFac.deploy("0x0850aC7A0Db7468816f5EbaFb0F10f6E1208126B", "0x0480d37a1a94eaada396833ca0c616fe9b8dfa1a9c29e3ffc58520c8164d82c6");
+    await merkleRootFac.deploy("0x0850aC7A0Db7468816f5EbaFb0F10f6E1208126B", "0x0480d37a1a94eaada396833ca0c616fe9b8dfa1a9c29e3ffc58520c8164d82c6"); //used the token from my last project
 
     //address _token, bytes32 _merkleRoot
 
@@ -24,4 +24,5 @@ main()
         process.exit(1);
     });
 
-// ContractAddress: 
+// ContractAddress: 0xef3B57eaEde0D1869d746bF559e358544B71A0f1 [verified]
+// https://sepolia-blockscout.lisk.com/address/0xef3B57eaEde0D1869d746bF559e358544B71A0f1
